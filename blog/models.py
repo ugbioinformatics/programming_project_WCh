418287fec8620d986f11edc0fec3dcdc3d59f77c
gotowiece <125959393+gotowiece@users.noreply.github.com>
2023-03-28 10:48:30 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -43,6 +43,7 @@ class Post(models.Model):
     molwt = models.FloatField(blank=True, null=True)
     sequence = models.TextField(default='') 
     charge = models.FloatField(blank=True, null=True) 
+    pKscale = models.CharField(max_length=200,default='')
     
 
     def __str__(self):

fe19f63e37257fcdcfd190371ed6f95c3f52c0dc
gotowiece <125959393+gotowiece@users.noreply.github.com>
2023-04-18 09:34:44 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -105,6 +105,10 @@ Wszystkie pola są opcjonalne, ponieważ mają ustawioną wartość required=Fal
                
 class Database_form(forms.Form):
     title = forms.CharField(widget=forms.TextInput(attrs={'size': 40, 'maxlenght': 40}))
+    database_choice = [
+        ('Uniprot', 'Uniprot'),
+        ('PDB', 'PDB'),
+    ]
     id = forms.CharField(widget=forms.TextInput(attrs={'size': 40, 'maxlenght': 200}), label='Uniprot ID')
     def clean(self):
         cleaned_data = super(Database_form, self).clean()

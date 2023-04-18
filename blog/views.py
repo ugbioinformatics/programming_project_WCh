46bf6302f860942e202878d6bc4a6c7cc72bcf61
domalewskag <125960169+domalewskag@users.noreply.github.com>
2023-04-18 11:27:11 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -386,10 +386,10 @@ def edit_peptide(request, pk):
 def database(request):
     if request.method == 'POST':
         form = Database_form(request.POST) 
-        if form.is_valid():
-            database_id = form.cleaned_data["id"] 
-            choice = form.cleaned_data["database"]
-            title = form.cleaned_data["title"] 
+        if form.is_val():
+            database_id = form.cleaned_data["database_id"] 
+            choice = form.cleaned_data["database_choice"]
+            title = form.cleaned_data["database_title"] 
             if request.user.is_authenticated:
                 post = Post(database_id=database_id, database_choice=choice, title=title, author=request.user)
             else:

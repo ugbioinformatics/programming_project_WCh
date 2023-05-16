abd8bc287dd863092e6a4b03ac27475287a7d84f
Cezary Czaplewski <100878859+cc1781@users.noreply.github.com>
2023-05-16 12:05:05 +0200

    Update utils.py

--- blog/utils.py
+++ blog/utils.py
@@ -41,7 +41,7 @@ def search_uniprot(query: str, format: str, fields: Optional[List[str]]=[], revi
     if response.ok:
         data = response.text
         logger.warning("test logowanie")
-        logger.warning(data)
+        logger.warning(data=="")
         if data.startswith("<!doctype html>"):
             return None
         if format == 'list':
